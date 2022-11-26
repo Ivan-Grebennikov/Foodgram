38e9e30933e429994824b1c83829fa0c55d52683
Ivan-Grebennikov <gre-ivan@yandex.ru>
2022-11-26 20:45:25 +0300

    [update] add github action workflow

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo docker compose pull
 sudo docker compose up -d
 sudo docker compose exec backend python manage.py migrate
 sudo docker compose exec backend python manage.py loaddata ../data/fixtures.json
