b783f21158cb4284f3061ceeb7eba399f128d0e8
Ivan-Grebennikov <gre-ivan@yandex.ru>
2022-11-29 02:36:52 +0300

    [update] make db volume persistent between docker containers restart

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -64,8 +64,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.getenv('DB_HOST', 'db'),
